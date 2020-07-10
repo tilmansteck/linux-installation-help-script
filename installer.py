ee9446aad47f154bba009b7dda50fe28c2f0c540
Tilman Steck <52348116+tilmansteck@users.noreply.github.com>
2020-07-10 21:39:34 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -2,19 +2,62 @@ import os
 import time
 
 
+# default installer
 def installer():
-    print("[*] Starting installation...")
-
-    os.system("apt-get update")
-    os.system("apt-get upgrade")
+    print("[*] Starting installation...\n\n")
 
     os.system("apt-get install sudo")
-    os.system("apt-get install nano")
-    os.system("apt-get install python")
+
+    os.system("sudo apt-get update")
+    os.system("sudo apt-get upgrade")
 
     time.sleep(1)
 
-    print("[*] Installation done!")
 
-    
-    
+# installer for important programms
+def programm_installer():
+    nano = input("Do you want to install console text editor 'nano'? y or n: ")
+
+    # nano installation
+    if nano == "y" or nano == "Y":
+        os.system("sudo apt-get install nano")
+
+    else:
+        pass
+
+    # python installation
+    python = input("Do you want to download 'python'? y or n: ")
+
+    if python == "y" or python == "Y":
+        os.system("sudo apt-get install python")
+
+    else:
+        pass
+
+    # screen installation
+    screen = input("Do you want to download 'screen'? y or n: ")
+
+    if screen == "y" or screen == "Y":
+        os.system("sudo apt-get install screen")
+
+    else:
+        pass
+
+    # htop installation
+    htop = input("Do you want to download taskmanager 'htop'? y or n: ")
+
+    if htop == "y" or htop == "Y":
+        os.system("sudo apt-get install htop")
+
+    else:
+        pass
+
+    # installation done
+    print("\n\n[*] To complete the installation please write 'done'.")
+    end = input()
+
+    if end == "done":
+        exit()
+
+    else:
+        exit()
