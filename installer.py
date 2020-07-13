57973cf58660c84d72109d9c103c6d76bce89ebc
Tilman Steck <52348116+tilmansteck@users.noreply.github.com>
2020-07-13 16:49:10 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -1,24 +1,73 @@
 import os
 import time
-import threading
+import platform
+
+
+# class for console colors (best success on linux based systems)
+class color:
+    END = '\33[0m'
+    BOLD = '\33[1m'
+    ITALIC = '\33[3m'
+    URL = '\33[4m'
+    BLINK = '\33[5m'
+    BLINK2 = '\33[6m'
+    SELECTED = '\33[7m'
+
+    BLACK = '\33[30m'
+    RED = '\33[31m'
+    GREEN = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE = '\33[34m'
+    VIOLET = '\33[35m'
+    BEIGE = '\33[36m'
+    WHITE = '\33[37m'
+
+    BLACKBG = '\33[40m'
+    REDBG = '\33[41m'
+    GREENBG = '\33[42m'
+    YELLOWBG = '\33[43m'
+    BLUEBG = '\33[44m'
+    VIOLETBG = '\33[45m'
+    BEIGEBG = '\33[46m'
+    WHITEBG = '\33[47m'
+
+    GREY = '\33[90m'
+    RED2 = '\33[91m'
+    GREEN2 = '\33[92m'
+    YELLOW2 = '\33[93m'
+    BLUE2 = '\33[94m'
+    VIOLET2 = '\33[95m'
+    BEIGE2 = '\33[96m'
+    WHITE2 = '\33[97m'
+
+    GREYBG = '\33[100m'
+    REDBG2 = '\33[101m'
+    GREENBG2 = '\33[102m'
+    YELLOWBG2 = '\33[103m'
+    BLUEBG2 = '\33[104m'
+    VIOLETBG2 = '\33[105m'
+    BEIGEBG2 = '\33[106m'
+    WHITEBG2 = '\33[107m'
 
 
 # script start banner
 def banner():
-    print("  __       __  .__   __.  __    __  ___   ___ ")
-    print(" |  |     |  | |  \ |  | |  |  |  | \  \ /  / ")
-    print(" |  |     |  | |   \|  | |  |  |  |  \  V  /  ")
-    print(" |  |     |  | |  . `  | |  |  |  |   >   <   ")
-    print(" |  `----.|  | |  |\   | |  `--'  |  /  .  \  ")
-    print(" |_______||__| |__| \__|  \______/  /__/ \__\ ")
+    os.system("clear")
+
+    print(color.YELLOW2 + "  __       __  .__   __.  __    __  ___   ___ ")
+    print(color.YELLOW2 + " |  |     |  | |  \ |  | |  |  |  | \  \ /  / ")
+    print(color.YELLOW2 + " |  |     |  | |   \|  | |  |  |  |  \  V  /  ")
+    print(color.YELLOW2 + " |  |     |  | |  . `  | |  |  |  |   >   <   ")
+    print(color.YELLOW2 + " |  `----.|  | |  |\   | |  `--'  |  /  .  \  ")
+    print(color.YELLOW2 + " |_______||__| |__| \__|  \______/  /__/ \__\ ")
     print(" ")
-    print("** Linux installation manager by Tilman Steck **")
+    print(color.YELLOW2 + "** Linux installation manager by Tilman Steck **")
     print(" ")
 
 
 # default installer
 def installer():
-    print("[*] Starting installation...\n\n")
+    print(color.YELLOW2 + "\n[*] Starting installation...\n" + color.GREEN2)
 
     os.system("apt-get install sudo")
 
@@ -30,7 +79,7 @@ def installer():
 
 # installer for important programms
 def programm_installer():
-    nano = input("\nDo you want to install console text editor 'nano'? y or n: ")
+    nano = input(color.BLUE2 + "\nDo you want to install console text editor 'nano'? y or n: " + color.GREEN2)
 
     # nano installation
     if nano == "y" or nano == "Y":
@@ -40,7 +89,7 @@ def programm_installer():
         pass
 
     # python installation
-    python = input("\nDo you want to download 'python'? y or n: ")
+    python = input(color.BLUE2 + "\nDo you want to download 'python'? y or n: " + color.GREEN2)
 
     if python == "y" or python == "Y":
         os.system("sudo apt-get install python")
@@ -49,8 +98,17 @@ def programm_installer():
     else:
         pass
 
+    # git installation
+    python = input(color.BLUE2 + "\nDo you want to download 'git'? y or n: " + color.GREEN2)
+
+    if python == "y" or python == "Y":
+        os.system("sudo apt-get install git")
+
+    else:
+        pass
+
     # screen installation
-    screen = input("\nDo you want to download 'screen'? y or n: ")
+    screen = input(color.BLUE2 + "\nDo you want to download 'screen'? y or n: " + color.GREEN2)
 
     if screen == "y" or screen == "Y":
         os.system("sudo apt-get install screen")
@@ -59,7 +117,7 @@ def programm_installer():
         pass
 
     # htop installation
-    htop = input("\nDo you want to download taskmanager 'htop'? y or n: ")
+    htop = input(color.BLUE2 + "\nDo you want to download taskmanager 'htop'? y or n: " + color.GREEN2)
 
     if htop == "y" or htop == "Y":
         os.system("sudo apt-get install htop")
@@ -68,11 +126,12 @@ def programm_installer():
         pass
 
     # installation done
-    print("\n\n[*] To complete the installation please write 'done'.")
+    print(color.BLUE2 + "\n\n[*] To complete the installation please write 'done'." + color.GREEN2)
     end = input()
 
     if end == "done":
-        exit()
+        print(color.BLUE2 + "\n[*] Installation is done now!\n" + color.END)
+        os._exit(1)
 
     else:
         exit()
@@ -80,23 +139,29 @@ def programm_installer():
 
 banner()
 
-# use installer or not
-i1 = input("\nDo you want to use the default linux installer? y or n: ")
+if platform.system() == "Linux":
 
-if i1 == "y" or i1 == "Y":
-    installer()
+    # use installer or not
+    i1 = input(color.BLUE2 + "\nDo you want to use the default linux installer? y or n: ")
 
-else:
-    exit()
+    if i1 == "y" or i1 == "Y":
+        installer()
 
-# install also important programs
-i2 = input("\nDo you want to install a few important programs? y or n:")
+    else:
+        pass
 
-if i2 == "y" or i2 == "Y":
-    programm_installer()
+    # install also important programs
+    i2 = input(color.BLUE2 + "\nDo you want to install a few important programs? y or n:")
 
-else:
+    if i2 == "y" or i2 == "Y":
+        programm_installer()
+
+    else:
+        pass
+
+    print(color.BLUE2 + "\n[*] Installation is done now! \n" + color.END)
     os._exit(1)
 
-print("\n[*] Installation is done now!")
-exit()
+else:
+    print(color.RED2 + "\n[*] This installation manager only runs on Linux Based Systems!" + color.END)
+    os._exit(1)
